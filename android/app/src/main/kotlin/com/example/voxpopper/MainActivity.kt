1a5bd9abd6b236a3dbde19b47f798b678bf4dd04
Donlumyd <farukolatunji83@gmail.com>
2021-02-09 17:15:11 +0100

    Verb

---
+++ android/app/src/main/kotlin/com/example/voxpopper/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.voxpopper
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:voxpopper/loginScreen.dart';
+import 'package:voxpopper/registrationScreen.dart';
+import 'package:voxpopper/welcomeScreen.dart';
+
+ void main (){
+   runApp(
+       Verb(
+   ),
+   );
+ }
+
+ class Verb extends StatelessWidget {
+   @override
+   Widget build(BuildContext context) {
+     return MaterialApp(
+     theme: ThemeData(
+         accentColor: Colors.lightBlue[800],
+         primaryColor: Colors.white,
+         textTheme: TextTheme(
+            bodyText1: TextStyle(
+                color: Colors.blue
+            ),
+         ),
+     ),
+
+       initialRoute: WelcomeScreen.id,
+       routes: {
+       WelcomeScreen.id: (context) => WelcomeScreen(),
+         LoginScreen.id: (context) => LoginScreen(),
+         RegistrationScreen.id: (context) => RegistrationScreen(),
+
+       },
+     );
+
+   }
+ }
+
