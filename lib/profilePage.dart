0bf11dd30ae0a99a836349e8c0b35b66751b1eef
Donlumyd <farukolatunji83@gmail.com>
2021-03-05 22:23:26 +0100

    3/5

--- lib/profilePage.dart
+++ lib/profilePage.dart
@@ -1,24 +1,52 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
-class ProfilePage extends StatelessWidget {
-
+class ProfilePage extends StatefulWidget {
   static String id = 'profilePage';
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-       title: Text ('Profile Page'),
-    ),
+  _ProfilePageState createState() => _ProfilePageState();
+}
 
-    body: Row(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-      Image.asset(name);
+class _ProfilePageState extends State<ProfilePage> {
 
-    TextField()
-    ],
-    );
+  late File _image;
+  final picker = ImagePicker();
 
+  Future getImage() async {
+    final pickedFile = await picker.getImage(source: ImageSource.camera);
+
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+        print('No image selected.');
+      }
+    });
+
+    @override
+    Widget build(BuildContext context) {
+     return Scaffold(
+       appBar: AppBar(
+         title: Text('ProfilePage'),
+       ),
+
+       body: Center(
+       child: (_image == null)
+      ? Text('No image selected.')
+          : Image.file(_image),
+      ),
+      floatingActionButton: FloatingActionButton(
+      onPressed: getImage,
+      tooltip: 'Pick Image',
+      child: Icon(Icons.add_a_photo),
+      ),
+     );
+    }
   }
+
+
+
+
 }
